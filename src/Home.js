5ac368fd3e75b685d2ebbf7560113f5dd6092814
Alexis Velluet <alexis.velluet@outlook.com>
2023-06-27 17:08:55 +0200

    induced fetch json serer on useEffect

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,7 @@ import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const [name, setName] = useState ("Mario")
 
@@ -16,14 +12,19 @@ const Home = () => {
   }
 
   useEffect(() => {
-    console.log("use Effect");
-    console.log(name)
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      setBlogs(data)
+    })
+  }, []);
 
   return (
   <div className="home">
-    < BlogList blogs={blogs} title="All blogs !" handleDelete={handleDelete}/>
-    < BlogList blogs={blogs.filter((blogs) => blogs.author === 'mario')} title="Mario's blog" handleDelete={handleDelete}/>
+    {blogs && < BlogList blogs={blogs} title="All blogs !" handleDelete={handleDelete} />}
+    {blogs && < BlogList blogs={blogs.filter((blogs) => blogs.author === 'mario')} title="Mario's blog" handleDelete={handleDelete} />}
     <button onClick={() => {setName("luigi")}}>Change Name</button>
     <p>{name}</p>
   </div>
