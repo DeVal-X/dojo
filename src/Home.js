6cef6800e639a286c7c72b93a57e29352bfd7caf
Alexis Velluet <alexis.velluet@outlook.com>
2023-06-27 17:52:14 +0200

    introduced a conditional loading message during the fetch

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,9 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   const [name, setName] = useState ("Mario")
-
-  const handleDelete = (id) =>{
-    const newBlogs = blogs.filter(blogs => blogs.id !== id);
-    setBlogs(newBlogs);
-  }
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
@@ -17,16 +13,21 @@ const Home = () => {
       return res.json();
     })
     .then(data => {
-      setBlogs(data)
+      setBlogs(data);
+      setIsPending(false);
     })
   }, []);
 
   return (
   <div className="home">
-    {blogs && < BlogList blogs={blogs} title="All blogs !" handleDelete={handleDelete} />}
-    {blogs && < BlogList blogs={blogs.filter((blogs) => blogs.author === 'mario')} title="Mario's blog" handleDelete={handleDelete} />}
+    {isPending && <div>loading ... </div>}
+
+    {blogs && < BlogList blogs={blogs} title="All blogs !"  />}
+    {blogs && < BlogList blogs={blogs.filter((blogs) => blogs.author === 'mario')} title="Mario's blog" />}
     <button onClick={() => {setName("luigi")}}>Change Name</button>
     <p>{name}</p>
+
+
   </div>
   );
 }
