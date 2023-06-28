d3720707d75f60f98f1ac233226bb9c9dd022d8a
Alexis Velluet <alexis.velluet@outlook.com>
2023-06-28 15:57:22 +0200

    introduced react router 5, deleted the change name button

--- src/Home.js
+++ src/Home.js
@@ -5,20 +5,15 @@ import useFetch from './useFetch';
 
 const Home = () => {
   const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
-  const [name, setName] = useState ("Mario")
 
   return (
-  <div className="home">
-    {isPending && <div>loading ... </div>}
-    {error && <div> {error} </div>}
+    <div className="home">
+      {isPending && <div>loading ... </div>}
+      {error && <div> {error} </div>}
 
-    {blogs && < BlogList blogs={blogs} title="All blogs !"  />}
-    {blogs && < BlogList blogs={blogs.filter((blogs) => blogs.author === 'mario')} title="Mario's blog" />}
-    <button onClick={() => {setName("luigi")}}>Change Name</button>
-    <p>{name}</p>
-
-
-  </div>
+      {blogs && < BlogList blogs={blogs} title="All blogs !"  />}
+      {blogs && < BlogList blogs={blogs.filter((blogs) => blogs.author === 'mario')} title="Mario's blog" />}
+    </div>
   );
 }
 
