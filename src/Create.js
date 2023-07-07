b4bf19da232e45ca89f4f181ae166ab0ce7c8f54
Alexis Velluet <alexis.velluet@outlook.com>
2023-07-07 15:48:21 +0200

    made an input form in the create blog component with state to follow the value of the fields

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,43 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [ title, setTitle] = useState('');
+  const [ body, setBody] = useState('');
+  const [ author, setAuthor] = useState('mario');
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
+      <form >
+        <label>blog title</label>
+        <input
+          type="text"
+          required
+          value={ title }
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>blog body :</label>
+        <textarea
+          required
+          value={ body }
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+
+        <label>Blog author</label>
+        <select
+          value={ author }
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+
+        <button> Add blog</button>
+      </form>
+      <p>{ title }</p>
+      <p>{ body }</p>
+      <p>{ author }</p>
     </div>
   );
 }
